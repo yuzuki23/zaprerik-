1376670e4518df2155a1f2254cae1c58eb9d5126
yuzuki23 <yuzuki23@users.noreply.github.com>
2026-08-07 18:00:00 +0300

    Менеджер: ссылки на наш репозиторий, добавлены .service (version/hosts/ipset)

--- service.bat
+++ service.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 set "LOCAL_VERSION=1.0.4"
 
 :: External commands
@@ -376,9 +376,9 @@ chcp 437 > nul
 cls
 
 :: Set current version and URLs
-set "GITHUB_VERSION_URL=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/main/.service/version.txt"
-set "GITHUB_RELEASE_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/tag/"
-set "GITHUB_DOWNLOAD_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/latest"
+set "GITHUB_VERSION_URL=https://raw.githubusercontent.com/yuzuki23/zaprerik-/main/.service/version.txt"
+set "GITHUB_RELEASE_URL=https://github.com/yuzuki23/zaprerik-/releases/tag/"
+set "GITHUB_DOWNLOAD_URL=https://github.com/yuzuki23/zaprerik-/releases/latest"
 
 :: Get the latest version from GitHub
 for /f "delims=" %%A in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri \"%GITHUB_VERSION_URL%\" -Headers @{\"Cache-Control\"=\"no-cache\"} -UseBasicParsing -TimeoutSec 5).Content.Trim()" 2^>nul') do set "GITHUB_VERSION=%%A"
@@ -466,7 +466,7 @@ echo:
 tasklist /FI "IMAGENAME eq AdguardSvc.exe" | find /I "AdguardSvc.exe" > nul
 if !errorlevel!==0 (
     call :PrintRed "[X] Adguard process found. Adguard may cause problems with Discord"
-    call :PrintRed "https://github.com/Flowseal/zapret-discord-youtube/issues/417"
+    call :PrintRed "https://github.com/yuzuki23/zaprerik-/issues/417"
 ) else (
     call :PrintGreen "Adguard check passed"
 )
@@ -476,7 +476,7 @@ echo:
 sc query | findstr /I "Killer" > nul
 if !errorlevel!==0 (
     call :PrintRed "[X] Killer services found. Killer conflicts with zapret"
-    call :PrintRed "https://github.com/Flowseal/zapret-discord-youtube/issues/2512#issuecomment-2821119513"
+    call :PrintRed "https://github.com/yuzuki23/zaprerik-/issues/2512#issuecomment-2821119513"
 ) else (
     call :PrintGreen "Killer check passed"
 )
@@ -989,7 +989,7 @@ chcp 437 > nul
 cls
 
 set "listFile=%~dp0lists\ipset-all.txt"
-set "url=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/refs/heads/main/.service/ipset-service.txt"
+set "url=https://raw.githubusercontent.com/yuzuki23/zaprerik-/refs/heads/main/.service/ipset-service.txt"
 
 echo Updating ipset-all...
 
@@ -1022,7 +1022,7 @@ chcp 437 > nul
 cls
 
 set "hostsFile=%SystemRoot%\System32\drivers\etc\hosts"
-set "hostsUrl=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/refs/heads/main/.service/hosts"
+set "hostsUrl=https://raw.githubusercontent.com/yuzuki23/zaprerik-/refs/heads/main/.service/hosts"
 set "tempFile=%TEMP%\zapret_hosts.txt"
 set "needsUpdate=0"
 
@@ -1168,7 +1168,7 @@ exit /b
 where %1 >nul 2>&1
 if %errorLevel% neq 0 (
     echo [ERROR] %1 not found in PATH
-    echo Fix your PATH variable with instructions here https://github.com/Flowseal/zapret-discord-youtube/issues/7490
+    echo Fix your PATH variable with instructions here https://github.com/yuzuki23/zaprerik-/issues/7490
     pause
     exit /b 1
 )
